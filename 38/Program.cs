57e0e8d581ff549d2ce9441a915ef68cf5fed8d8
Roman <igoshinr.estho@gmail.com>
2022-08-10 06:24:30 +0300

    38

---
+++ 38/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+double max = 0;
+double min = 100;
+double[] arr = new double[n];
+for (int i = 0; i < n; i++) 
+{
+    arr[i] = new Random().Next(1,100);
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+}
+Console.WriteLine("[" + string.Join(", ", arr) + "]");
+Console.WriteLine(max - min);
+
