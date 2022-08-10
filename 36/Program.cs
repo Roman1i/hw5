b8a02da5bee6e144339d198c18933674d29bafa5
Roman <igoshinr.estho@gmail.com>
2022-08-10 06:01:10 +0300

    36

---
+++ 36/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int sum = 0;
+for (int i = 0; i < n; i++) 
+{
+    arr[i] = new Random().Next(1,100);
+    if (i % 2 == 0) sum += arr[i];
+}
+//Console.WriteLine("[" + string.Join(", ", arr) + "]");
+Console.WriteLine(sum);
